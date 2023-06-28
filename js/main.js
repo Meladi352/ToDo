ce0684a541f1957223c558fe7ab34ee45a6f1df4
Kseniia Surmenkova <surmenkovakseniia@gmail.com>
2023-06-28 21:45:00 +0300

    The basic markup has been created

---
+++ js/main.js
@@ -0,0 +1,60 @@
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const tasksList = document.querySelector('#tasksList');
+const emptyList = document.querySelector('#emptyList');
+
+form.addEventListener('submit', addTask);
+
+tasksList.addEventListener('click', deleteTask);
+
+tasksList.addEventListener('click', doneTask);
+
+function addTask (event){
+    event.preventDefault();
+    
+   const taskText = taskInput.value;
+   const taskHTML = `
+            <li class="list-group-item d-flex justify-content-between task-item">
+                <span class="task-title">${taskText}</span>
+                <div class="task-item__buttons">
+                    <button type="button" data-action="done" class="btn-action">
+                        <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                    </button>
+                    <button type="button" data-action="delete" class="btn-action">
+                        <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                </button>
+            </div>
+        </li>`;
+
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+
+    taskInput.value = "";
+    taskInput.focus();
+
+    if(tasksList.children.length > 1) {
+        emptyList.classList.add('none');
+    }
+}
+
+function deleteTask(event){
+    
+    if (event.target.dataset.action === 'delete') {
+        
+        const parenNode = event.target.closest('.list-group-item');
+        parenNode.remove();
+    }
+
+    if(tasksList.children.length === 1) {
+        emptyList.classList.remove('none');
+    }
+}
+
+function doneTask(event){
+    if (event.target.dataset.action === "done"){
+      const parenNode = event.target.closest('.list-group-item'); 
+      
+     const taskTitle = parenNode.querySelector('.task-title');
+     taskTitle.classList.toggle('task-title--done');
+    }
+}
+
